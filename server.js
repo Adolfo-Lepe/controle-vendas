4874c461671cfa0a326876a8ed13d1a2e820cc89
Adolfo-Lepe <adolfolepe@hotmail.com>
2026-09-16 13:11:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -107,4 +107,4 @@ app.get(['/api/pedidos', '/api/vendas'], async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Rodando na porta ${PORT}));
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
